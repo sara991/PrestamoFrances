cdbb7c50316ab98853c4e8b88bdf6d651b514f42
sara991 <yamis704@gmail.com>
2020-10-03 21:42:17 -0600

    rutas

---
+++ index.js
@@ -0,0 +1,23 @@
+// Importar express
+// -----variable para importar modulo, express es un servidor web escuchar peticiones
+//y dar respuestas a las peticiones
+const express = require("express");
+
+// crear un servidor express en app
+const app= express();
+
+
+// Crear una ruta para /
+// Información sobre los verbos HTTP
+// ----get peticion para un servidor ( require= encabezados, response=respuesta que da el servidor)
+
+app.get("/", (req, res, next) => {
+ res.send("Bienvenido");
+});
+
+
+
+// Inicializar el servidor express en un puerto en específico
+app.listen(5000, () => {
+  console.log("Servidor ejecutándose en el puerto 8080");
+});
