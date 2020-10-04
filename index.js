8b9d9fd8504a0b05e69200b11247b8326ea30e80
sara991 <yamis704@gmail.com>
2020-10-03 22:59:45 -0600

    Creacion de layout principal del sitio con un header, container y un footer prinicpal

--- index.js
+++ index.js
@@ -3,19 +3,29 @@
 //y dar respuestas a las peticiones
 const express = require("express");
 
+//iMPORTAR HANDELEBARS COMO PLANTILLA
+const exphbs = require("express-handlebars");
+
+
+
 // crear un servidor express en app
 const app= express();
+//  INDICAR A EXPRESS USAR HANDLEBARS COMO PLANTILLA
+//
+app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
 
-
+app.set("view engine", "hbs");
 // Crear una ruta para /
-
+//render cargar 
 app.get("/", (req, res, next) => {
- res.send("Bienvenido");
+ res.render("formulario_prestamo");
 });
 
 
 
 // Inicializar el servidor express en un puerto en específico
 app.listen(5000, () => {
-  console.log("Servidor ejecutándose en el puerto 8080");
+  console.log("Servidor ejecutándose en el puerto 5000");
 });
+
+
